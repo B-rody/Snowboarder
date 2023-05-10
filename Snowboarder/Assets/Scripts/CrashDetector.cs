3dc9e677c8b5e0ba91c071cfa5579ef3a72455ff
B-rody <brschulk@outlook.com>
2023-05-09 19:26:23 -0700

    Added level restart upon completion and crash. Also added corresponding particle effects

---
+++ Snowboarder/Assets/Scripts/CrashDetector.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CrashDetector : MonoBehaviour
+{
+    [SerializeField] float reloadSceneDelay = 1f;
+    [SerializeField] ParticleSystem crashEffect;
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+        if (other.tag == "Ground")
+        {
+            crashEffect.Play();
+            Invoke("ReloadScene", reloadSceneDelay);
+        }
+    }
+
+    void ReloadScene() 
+    {
+        SceneManager.LoadScene(0);
+    }
+}
